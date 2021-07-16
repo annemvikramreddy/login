9f18a62d4083ae5500b3ba65b8de0e396b978ce1
vikram <annemvikramreddy@gmail.com>
2021-07-16 13:50:34 +0000

    added login

---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-sections">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="cart-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
